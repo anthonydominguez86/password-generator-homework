38694a22c626ffabb2e325a129f89becf2b2b6a7
anthonydominguez86 <adominguez11186@gmail.com>
2020-12-30 13:32:25 -0500

    console log created

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// alert ("Welcome to the password generator for security purposes lets create a new password.");
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+for (var i = 0; i < 10; i++) {
+var generateBtn = Math.floor(Math.random() * 10) + 1;
+// Display in console
+console.log(generateBtn);
+}
 
 // Write password to the #password input
 function writePassword() {
