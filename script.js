b496162ba94a9bf62c14da9376b46c9d81712fb8
anthonydominguez86 <adominguez11186@gmail.com>
2021-01-05 16:53:46 -0500

    adding variables and layout of README

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-// alert ("Welcome to the password generator for security purposes lets create a new password.");
+alert ("Welcome to the password generator for security purposes lets create a new password.");
+
+// The Password generator will provide a password with 8-128 characters based on criteria the user specifies.
+
+//Assignment Code + Event Listener to prompt questions when button pushed
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+// Various Arrays 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+// Variable Declaration 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -17,6 +37,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
