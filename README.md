481081b750094162bac63790cd6bd26ff74d72b4
anthonydominguez86 <adominguez11186@gmail.com>
2021-01-06 14:59:45 -0500

    README file completed Java completed

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Password Generator Instructions Create Secure Password
+# The Project: Password Generator Instructions Create Secure Password
+
 * WHEN I click the button to generate a password
 * THEN I am presented with a series of prompts for password criteria
 * WHEN prompted for password criteria
@@ -14,4 +15,20 @@
 * WHEN the password is generated
 * THEN the password is either displayed in an alert or written to the page
 
-Above is the criteria for the password generator it was neccesary to create the code in Java to complete the actions listed above. At first glance this seemed like a simple task but by no means was this a simple task at all. This takes alot of concepts and variables to be written in order to complete the above tasks. I had originally approched this with a totally different mindstate and a completely different way of writing the code originally but with alot of research I found using arrays to be the most efficient method to complete the tasks. 
+Above is the criteria for the password generator it was neccesary to create the code in Java to complete the actions listed above. At first glance this seemed like a simple task but by no means was this a simple task at all. This takes alot of concepts and variables to be written in order to complete the above tasks. I had originally approached this with a totally different mindstate and a completely different way of writing the code but with alot of research I found this method to be the most effective way of completing the tasks and also understanding how the code was working. 
+
+# Lessons:
+
+The lessons I learned from this assignment are vast. We were given a set HTML and CSS but were expected to create the Java in order to make the password generate as mentioned above. I learned how to use the console log efficiently to test the code that I was writing as seen in generate password 2 (screenshot) I took a screenshot of how I was able to test a for loop with the console log. I learned how to write variables that I later could plug into formulas that would set parameters to generate a password. Also I learned how to loop in a while function that would be used in the set parameters were not met i.e. password length too short or too long. I learned that I can use the variables to be confirmed and not confirmed and then take that data to place into the password. This assignment was not an easy assignment the complexity of the Java code seemed approachable at first but it quickly became in my opinion quite complex. 
+
+# Takeaways:
+
+What I took from this assignment was how hard Java can be and how many ways a simple request can be made difficult. It also showed me that there is more than one way to get the assigment completed a for loop could be used, Math.random needed to be included with Math.floor, I could have done it a completely different way and used sets of arrays and ran those through the variables. I took alot from this assignment and I know this was a simple task that is just one building block to a multitude of possibilites that Java offers. I can now look at a website and inspect the code to see its complexities and admire how it works and one day more fully understand and write the code for complex websites. 
+
+# Screenshots 
+
+Screen shots of the assignment are available in the password-generator-homework/screenshots folder.
+
+# Deployment URL
+
+* 
--- script.js
+++ script.js
@@ -1,32 +1,78 @@
-alert ("Welcome to the password generator for security purposes lets create a new password.");
+alert("Welcome to the password generator for security purposes let's create a new password.");
 
 // The Password generator will provide a password with 8-128 characters based on criteria the user specifies.
 
-//Assignment Code + Event Listener to prompt questions when button pushed
+//Assignment Code & Event Listener to prompt questions when button pushed
 document.querySelector("#generate").addEventListener("click", writePassword);
 
-// Various Arrays 
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-
-// Variable Declaration 
+//Variables
+var number = "0123456789";
+var specialChar = "$#@%^+=!%&,*+-./<>?~;";
+var alphaLower = "abcdefghijklmnopqrstuvwxyz";
+var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var confirmLength = "";
 var confirmSpecialCharacter;
 var confirmNumericCharacter;
 var confirmUpperCase;
 var confirmLowerCase;
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Prompt to confirm how many characters the user would like in their password
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
 
+  // Loop if answer is outside the parameters 
+  while (confirmLength <= 7 || confirmLength >= 129) {
+    alert("Password length must be between 8-128 characters try again");
+    var confirmLength = (prompt("How many characters would you like your password to contain?"));
+  }
 
-for (var i = 0; i < 10; i++) {
-var generateBtn = Math.floor(Math.random() * 10) + 1;
-// Display in console
-console.log(generateBtn);
+  // Repeat how many charactes the user will have  
+  alert(`Your password will have ${confirmLength} characters`);
+
+  // Set parameters of password 
+  var confirmSpecialCharacter = confirm("Click ok to confirm if you would like to include special characters");
+  var confirmNumericCharacter = confirm("Click ok to confirm if you would like to include numeric characters");
+  var confirmLowerCase = confirm("Click ok to confirm if you would like to include lowercase characters");
+  var confirmUpperCase = confirm("Click ok to confirm if you would like to include uppercase characters");
+
+  // Loop if answer is outside the parameters 
+  while (confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+    alert("You must choose at least one parameter");
+    var confirmSpecialCharacter = confirm("Click ok to confirm if you would like to include special characters");
+    var confirmNumericCharacter = confirm("Click ok to confirm if you would like to include numeric characters");
+    var confirmLowerCase = confirm("Click ok to confirm if you would like to include lowercase characters");
+    var confirmUpperCase = confirm("Click ok to confirm if you would like to include uppercase characters");
+  }
+
+  // Assign an action to the password parameters
+  var passwordCharacters = []
+
+  if (confirmSpecialCharacter) {
+    passwordCharacters = passwordCharacters += (specialChar)
+  }
+
+  if (confirmNumericCharacter) {
+    passwordCharacters = passwordCharacters += (number)
+  }
+
+  if (confirmLowerCase) {
+    passwordCharacters = passwordCharacters += (alphaLower)
+  }
+
+  if (confirmUpperCase) {
+    passwordCharacters = passwordCharacters += (alphaUpper)
+  }
+
+  console.log(passwordCharacters)
+
+  // Empty string to be filled based on for loop selecting random characters from the array
+  var randomPassword = ""
+
+  for (var i = 0; i < confirmLength; i++) {
+    randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(randomPassword)
+  }
+  return randomPassword;
 }
 
 // Write password to the #password input
@@ -35,5 +81,4 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
